84b896f6c240c278950a3ba82b9ed5da21054a36
Karin Norrlander <karin@barprego.se>
2019-10-02 14:37:42 +0200

    Commit program

--- Program.cs
+++ Program.cs
@@ -6,11 +6,41 @@ namespace Multifabriken
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hej och välkommen till Multifabriken");
+            Console.ReadKey();
+            Console.WriteLine("Du får här 5 alternativ att välja mellan");
+            Console.WriteLine("1: Se hela produktutbudet");
+            Console.WriteLine("2: Gör en beställning");
+            Console.WriteLine("3: Se hela din beställning");
+            Console.WriteLine("4: Ta bort en produkt från din beställning");
+            Console.WriteLine("5: Avsluta programmet");
+
+            Console.WriteLine("Nu kan du göra ditt val:");
+            int nr = Convert.ToInt32(Console.ReadLine());
+            
+            switch (nr)
+            {
+                case 1:
+                break;
+
+                case 2:
+                break;
+                
+                case 3:
+                break;
+                
+                case 4:
+                break;
+                
+                case 5:
+                break;
+
+                default:
+                break;
+            }
         }
-    }
-    class Pipe
-    {
-        
+
+
+
     }
 }
