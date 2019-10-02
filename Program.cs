35d390e2e83e0bff23180382e13e68e1139e46fc
wiseby <l.wiseby@gamil.com>
2019-10-02 11:51:38 +0200

    Init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multifabriken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
