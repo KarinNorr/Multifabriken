5d6e6db40c25ac8896e8f2c259b21190db11b1df
Filip Milbredt <filip_milbredt@hotmail.com>
2019-10-02 13:18:28 +0200

    Filip initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Multifabriken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Pipe
+    {
+        
+    }
+}
