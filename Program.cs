03450708010b9c416f44e22af399ab1a1be8243c
wiseby <l.wiseby@gamil.com>
2019-10-07 08:44:57 +0200

    Main run loop added.

--- Program.cs
+++ Program.cs
@@ -8,84 +8,115 @@ namespace Multifabriken
 
         List<Customer> customers = new List<Customer>();
 
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej och välkommen till Multifabriken");
-            Console.ReadKey();
-            Console.WriteLine("Du får här 5 alternativ att välja mellan");
-            Console.WriteLine("1: Se hela produktutbudet");
-            Console.WriteLine("2: Gör en beställning");
-            Console.WriteLine("3: Se hela din beställning");
-            Console.WriteLine("4: Ta bort en produkt från din beställning");
-            Console.WriteLine("5: Avsluta programmet");
+            bool createOrder = false;
+            bool run = true;
 
-            Console.WriteLine("Nu kan du göra ditt val:");
-            int nr = Convert.ToInt32(Console.ReadLine());
-            
-            switch (nr)
-            {
-                case 1:
-                //Här ska hela sortimentet skrivas ut
-                Console.WriteLine("Multifabriken producerar bilar, rör, havremjölk och godis,");
-              
-                Console.WriteLine("Bilar i X olika färger, X modeller. Du kan addera lyxpaket till alla varianter.");
-                Console.WriteLine("Rör finns i valfri diameter och i X antal material. Du väljer själv längden.");
-                Console.WriteLine("Havremjölk med valfri fetthalt och mängd.");
-                break;
+            do
+            {    
+                Console.WriteLine("Hej och välkommen till Multifabriken");
+                Console.ReadKey();
+                Console.WriteLine("Du får här 5 alternativ att välja mellan");
+                Console.WriteLine("1: Se hela produktutbudet");
+                Console.WriteLine("2: Gör en beställning");
+                Console.WriteLine("3: Se hela din beställning");
+                Console.WriteLine("4: Ta bort en produkt från din beställning");
+                Console.WriteLine("5: Avsluta programmet");
 
-                case 2:
-                //Här ska kunden kunna göra sin beställning
-                Console.WriteLine("Skriv in din beställning här genom att välja produkt:");
-                Console.WriteLine("[1] Bil");
-                Console.WriteLine("[2] Rör");
-                Console.WriteLine("[3] Havremjölk");
-                Console.WriteLine("[4] Godis");
-                Console.Write(">>");
-
-                int productSelected = GetInt();
-
-                switch (productSelected)
+                Console.WriteLine("Nu kan du göra ditt val:");
+                int nr = Convert.ToInt32(Console.ReadLine());
+                
+                switch (nr)
                 {
                     case 1:
+                    //Här ska hela sortimentet skrivas ut
+                    Console.WriteLine("Multifabriken producerar bilar, rör, havremjölk och godis,");
+                
+                    Console.WriteLine("Bilar i X olika färger, X modeller. Du kan addera lyxpaket till alla varianter.");
+                    Console.WriteLine("Rör finns i valfri diameter och i X antal material. Du väljer själv längden.");
+                    Console.WriteLine("Havremjölk med valfri fetthalt och mängd.");
                     break;
+
                     case 2:
+                    //Här ska kunden kunna göra sin beställning
+                    createOrder = true;
+                    Console.WriteLine("Skriv in din beställning här genom att välja produkt:");
+                    Console.WriteLine("[1] Bil");
+                    Console.WriteLine("[2] Rör");
+                    Console.WriteLine("[3] Havremjölk");
+                    Console.WriteLine("[4] Godis");
+                    Console.Write(">>");
+
+                    do
+                    {
+                        int productSelected = GetInt();
+                        
+                        switch (productSelected)
+                        {
+                            case 1:
+                            Console.WriteLine("Välj bilens egenskaper:");
+                            Console.Write("Färg: ");
+                            string color = Console.ReadLine();
+                            Console.WriteLine("Tillgängliga modeller >");
+                            Console.Write("Modell: ");
+                            string model = Console.ReadLine();
+                            Console.Write("Lyx [Ja/Nej]: ");
+                            bool fancy = GetInputBool(Console.ReadLine());
+                            
+                            break;
+                            case 2:
+
+                            break;
+                            case 3:
+                            break;
+                            case 4:
+                            break;
+                            default:
+                            Console.WriteLine("Fel vid inmatning");
+                            break;
+                        }
+
+                    }while(createOrder);
+
+
+                    Console.ReadKey();
+                    //vi ska spara in beställningen som ett objekt ur sin klass och in i korrekt lista
                     break;
+
+                    
                     case 3:
+                    //Här kan kunden få hela sin beställning utskriven
                     break;
+                    
                     case 4:
+                    //Detta alternativ tar bort en inmatad beställning
                     break;
+                    
+                    case 5:
+                    //Detta alternativ avslutar programmet
+                    run = false;
+                    Console.WriteLine("Programmet avslutas. Välkommen åter!");
+                    return;
+
                     default:
-                    Console.WriteLine("Fel vid inmatning");
                     break;
                 }
-
-
-
-
-                Console.ReadKey();
-                //vi ska spara in beställningen som ett objekt ur sin klass och in i korrekt lista
-                break;
-
-                
-                case 3:
-                //Här kan kunden få hela sin beställning utskriven
-                break;
-                
-                case 4:
-                //Detta alternativ tar bort en inmatad beställning
-                break;
-                
-                case 5:
-                //Detta alternativ avslutar programmet
-                Console.WriteLine("Programmet avslutas. Välkommen åter!");
-                return;
-
-                default:
-                break;
-            }
+            }while(run);
 
             
         }
+
+        public static bool GetInputBool(string input)
+        {
+            string lowerCaseInput = input.ToLower();
+            if (lowerCaseInput[0] == 'j') { return true; }
+            
+            else { return false; }
+        }
+
         public static int GetInt()
         {
                 int number = 0;
